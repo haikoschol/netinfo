5a7f16b3c1bde8cd921fe5acbcc17579de38fe8c
Haiko Schol <hs@haikoschol.com>
2012-12-02 06:07:11 -0800

    added dashboard url, template and view function

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+
+def dashboard(request):
+    interfaces = [{'name': 'eth0'}, {'name': 'eth1'}]
+    return render_to_response('dashboard.html', {'interfaces': interfaces})
+
--- netinfo/urls.py
+++ netinfo/urls.py
@@ -1,17 +1,5 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'netinfo.views.home', name='home'),
-    # url(r'^netinfo/', include('netinfo.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'dashboard.views.dashboard', name='dashboard'),
 )
